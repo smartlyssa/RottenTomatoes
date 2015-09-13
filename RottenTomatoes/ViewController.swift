6c09473b3acbafaf50d4748e56a45a7bd5d6e8e2
smartlyssa <lyssa@thegeekgoddess.net>
2015-09-12 23:46:43 -0700

    Rotten Tomatoes homework walkthrough

--- RottenTomatoes/ViewController.swift
+++ RottenTomatoes/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+private let CELL_NAME = "net.thegeekgoddess.rottentomatoes.moviecell"
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var movieTableView: UITableView!
+    var movies: NSArray?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies?.count ?? 0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+//        let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
+//        cell.textLabel?.text = "\(indexPath.row)"
+        
+        let movieDictionary = movies![indexPath.row] as! NSDictionary
+        let cell = tableView.dequeueReusableCellWithIdentifier(CELL_NAME) as! MovieCell
+        cell.movieTitleLabel.text = movieDictionary["title"] as? String
+        cell.movieDescriptionLabel.text = movieDictionary["synopsis"] as? String
+        
+        return cell
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        NSLog("TableView? \(movieTableView.frame)")
+        
+        let RottenTomatoesURLString = "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json"
+        
+        let request = NSMutableURLRequest(URL: NSURL(string:RottenTomatoesURLString)!)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data , response, error ) -> Void in
+            if let dictionary = try! NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.movies = dictionary["movies"] as? NSArray
+                    self.movieTableView.reloadData()
+                }
+                NSLog("\(dictionary)")
+            } else {
+            
+            }
+        }
+        task.resume()
+        
+    }    
+}
 
-
+class MovieCell:UITableViewCell {
+    
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieDescriptionLabel: UILabel!
+    
+    
 }
 
